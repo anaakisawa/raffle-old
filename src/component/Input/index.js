4c2ba56f326a5faf3c0a3287bed7e7ec96ecfa5c
anaakisawa <ana.akisawa@gmail.com>
2018-10-19 09:36:00 -0300

    create Input component

---
+++ src/component/Input/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+	render () {
+		return (
+			<div className="Input">
+				<input type = "text">
+			</div>
+
+			);
+	}
+
+}
+
+export default Input;
