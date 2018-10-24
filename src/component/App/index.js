e133b857195908e8c959e5774e591def0ea5bd1f
valtermartins1301 <valtermartins.1301@gmail.com>
2018-10-24 09:50:06 -0300

    fix input sintax

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header text='Nosso primeiro componente'>
+        </Header>
       </div>
     );
   }
