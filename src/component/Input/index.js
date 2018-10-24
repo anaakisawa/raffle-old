65374f02f65bd06b7e8a025ed995d9a9d5bc6049
anaakisawa <ana.akisawa@gmail.com>
2018-10-24 09:12:14 -0300

    corrigindo identacao

--- src/component/Input/index.js
+++ src/component/Input/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class Input extends Component {
-	render () {
-		return (
-			<div className="Input">
-				<input type = "text">
-			</div>
+  render () {
+    return (
+	  <div className="Input">
+	    P<input type = "text">
+	  </div>
 
 			);
-	}
+  }
 
 }
 
